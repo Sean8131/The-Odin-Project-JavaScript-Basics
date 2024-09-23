3b1acf3f142711a068f7b3f9493f83a63bf80ad5
Sean <seanselbyy@gmail.com>
2024-09-23 20:07:52 +1200

    Done some JS stuff

---
+++ function-basics.js
@@ -0,0 +1,33 @@
+const cl = console.log
+
+cl("Yo")
+
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
+
+cl(favoriteAnimal('Goat'));
+
+function typeOfToy(toy) {
+    return toy + " are my favourite type of toys."
+}
+
+cl(typeOfToy("Drones"));
+
+function add7(number) {
+    return number + 7
+}
+
+cl(add7(3));
+
+function multiply(number1, number2) {
+    return number1 * number2
+}
+
+cl(multiply(2,2));
+cl(multiply(2,4));
+cl(multiply(5,5));
+
+function capitalize(word) {
+    return 
+}
---
+++ variables-and-operators.js
@@ -0,0 +1,45 @@
+console.log("Heyy");
+
+let name = "John";
+let surname = "Doe";
+
+cl = console.log
+
+cl(name);
+cl(surname);
+
+let age = 11;
+cl(age);
+
+age = 54;
+
+cl(age);
+
+const pi = 3.14;
+
+cl(pi)
+
+cl((3+2)-76*(1+1));
+
+cl(50+50);
+cl(1+1+1+1+1+1);
+cl((4 + 6 + 9) / 77);
+
+let a = 10;
+cl(a);
+
+let max = 57;
+let actual = (max-13);
+let percentage = actual/max;
+
+const birthYear = 1989;
+let currentYear = 2024;
+let currentAge = currentYear-birthYear;
+
+let income = 100;
+let expenses = 50;
+let spending = 20;
+let savings = income-expenses-spending;
+let savingsRate = savings/income;
+cl(savingsRate);
+
